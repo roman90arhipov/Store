811742e81a106fdfd3704deded7f602330d137a0
roman90arhipov <58008262+roman90arhipov@users.noreply.github.com>
2019-11-28 21:26:15 +0200

    Update item.rb
    Co-Authored-By: Kson <alexey.kson@gmail.com>

--- item.rb
+++ item.rb
@@ -3,7 +3,7 @@ class Item
   attr_reader :name, :price, :weight
   attr_writer :name, :price, :weight
   
-  def initialize (name, price, weight)
+  def initialize(name, price, weight)
     @name = name
     @price = price
     @weight = weight
