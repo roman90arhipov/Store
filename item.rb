ddbb5c72af64702a074b82ecef593d2a50548b24
Roman Arhipov <roman90arhipov@gmail.com>
2019-11-28 21:40:05 +0200

    home_work_3

--- item.rb
+++ item.rb
@@ -14,5 +14,5 @@ end
 item_1 = Item.new("white_bread", 0.5, 400.0)
 puts "#{item_1.name}" + ": " + "#{item_1.price}" + ", " + "#{item_1.weight}" + "."
 
-item_2 = Item.new("apple_juice", "1$", "900g")
+item_2 = Item.new("apple_juice", 1, 900)
 puts item_2.name + ": " + item_2.price + ", " + item_2.weight + "."
