87b60270af0a3a33501f446d2f5ce998a37a004d
Roman Arhipov <roman90arhipov@gmail.com>
2019-11-28 21:12:48 +0200

    home_work_3

---
+++ item.rb
@@ -0,0 +1,18 @@
+class Item
+
+  attr_reader :name, :price, :weight
+  attr_writer :name, :price, :weight
+  
+  def initialize (name, price, weight)
+    @name = name
+    @price = price
+    @weight = weight
+  end
+end
+
+
+item_1 = Item.new("white_bread", "0.5$", "400g")
+puts "#{item_1.name}" + ": " + "#{item_1.price}" + ", " + "#{item_1.weight}" + "."
+
+item_2 = Item.new("apple_juice", "1$", "900g")
+puts item_2.name + ": " + item_2.price + ", " + item_2.weight + "."
